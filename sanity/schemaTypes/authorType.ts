6bebdcc93c991fb41231c2cdfade717e089745d0
celestialord01-eng <celestialord01@gmail.com>
2026-05-17 21:18:55 +0530

    Refactor authorType schema for improved clarity

--- sanity/schemaTypes/authorType.ts
+++ sanity/schemaTypes/authorType.ts
@@ -1,42 +1,45 @@
-import {UserIcon} from '@sanity/icons'
-import {defineArrayMember, defineField, defineType} from 'sanity'
+import { UserIcon } from '@sanity/icons'
+import { defineField, defineType } from 'sanity'
 
-export const defineType({
+export default defineType({
   name: 'author',
   title: 'Author',
   type: 'document',
   icon: UserIcon,
+
   fields: [
     defineField({
       name: 'name',
+      title: 'Name',
       type: 'string',
     }),
+
     defineField({
       name: 'slug',
+      title: 'Slug',
       type: 'slug',
       options: {
         source: 'name',
       },
     }),
+
     defineField({
       name: 'image',
+      title: 'Image',
       type: 'image',
       options: {
         hotspot: true,
       },
     }),
+
     defineField({
       name: 'bio',
+      title: 'Bio',
       type: 'array',
-      of: [
-        defineArrayMember({
-          type: 'block',
-          styles: [{title: 'Normal', value: 'normal'}],
-          lists: [],
-        }),
-      ],
+      of: [{ type: 'block' }],
     }),
   ],
+
   preview: {
     select: {
       title: 'name',
