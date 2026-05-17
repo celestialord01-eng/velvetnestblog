2a788c5426723b7e72ebd9204b49b9a9439df44c
celestialord01-eng <celestialord01@gmail.com>
2026-05-17 20:58:33 +0530

    Create About Page schema for Sanity

---
+++ sanity/schemaTypes/aboutPage.ts
@@ -0,0 +1,30 @@
+import { defineField, defineType } from "sanity"
+
+export default defineType({
+  name: "aboutPage",
+  title: "About Page",
+  type: "document",
+
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+
+    defineField({
+      name: "description",
+      title: "Description",
+      type: "text",
+    }),
+
+    defineField({
+      name: "image",
+      title: "About Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
