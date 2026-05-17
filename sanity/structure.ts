c703c5a880443582ffdcddb9f8b86d2a3eb804bd
celestialord01-eng <celestialord01@gmail.com>
2026-05-17 23:48:26 +0530

    Update structure titles and add new document types

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,15 +1,12 @@
-import type {StructureResolver} from 'sanity/structure'
+import type { StructureResolver } from "sanity/structure"
 
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
 export const structure: StructureResolver = (S) =>
   S.list()
-    .title('Blog')
+    .title("Content")
     .items([
-      S.documentTypeListItem('post').title('Posts'),
-      S.documentTypeListItem('category').title('Categories'),
-      S.documentTypeListItem('author').title('Authors'),
-      S.divider(),
-      ...S.documentTypeListItems().filter(
-        (item) => item.getId() && !['post', 'category', 'author'].includes(item.getId()!),
-      ),
+      S.documentTypeListItem("post").title("Posts"),
+      S.documentTypeListItem("category").title("Categories"),
+      S.documentTypeListItem("amazonFind").title("Amazon Finds"),
+      S.documentTypeListItem("hero").title("Homepage Hero"),
+      S.documentTypeListItem("aboutPage").title("About Page"),
     ])
