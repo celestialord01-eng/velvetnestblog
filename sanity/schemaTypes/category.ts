09d2d72dcc1f987c76394fd9af30adf119926ef2
celestialord01-eng <celestialord01@gmail.com>
2026-05-17 20:55:00 +0530

    Add category schema definition for Sanity

---
+++ sanity/schemaTypes/category.ts
@@ -0,0 +1,33 @@
+import { defineField, defineType } from "sanity"
+
+export default defineType({
+  name: "category",
+  title: "Category",
+  type: "document",
+
+  fields: [
+    defineField({
+      name: "title",
+      title: "Title",
+      type: "string",
+    }),
+
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    }),
+
+    defineField({
+      name: "image",
+      title: "Category Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    }),
+  ],
+})
