5b9257687e9e5680703c0c1c2c225c63a1f64a7c
celestialord01-eng <celestialord01@gmail.com>
2026-05-18 10:06:07 +0530

    Refactor category schema and add article count field
    Updated field names and titles for clarity. Added a new field for article count.

--- sanity/schemaTypes/category.ts
+++ sanity/schemaTypes/category.ts
@@ -1,33 +1,40 @@
-import { defineField, defineType } from "sanity"
+import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: "category",
-  title: "Category",
-  type: "document",
+  name: 'category',
+  title: 'Categories',
+  type: 'document',
 
   fields: [
     defineField({
-      name: "title",
-      title: "Title",
-      type: "string",
+      name: 'title',
+      title: 'Category Name',
+      type: 'string',
     }),
 
     defineField({
-      name: "slug",
-      title: "Slug",
-      type: "slug",
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
       options: {
-        source: "title",
+        source: 'title',
+        maxLength: 96,
       },
     }),
 
     defineField({
-      name: "image",
-      title: "Category Image",
-      type: "image",
+      name: 'image',
+      title: 'Category Image',
+      type: 'image',
       options: {
         hotspot: true,
       },
+    }),
+
+    defineField({
+      name: 'count',
+      title: 'Article Count',
+      type: 'number',
     }),
   ],
 })
