c898992217fd9b37b42aa8da47ab74355576d7c9
celestialord01-eng <celestialord01@gmail.com>
2026-05-21 17:40:27 +0530

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,14 +1,62 @@
-export default function TestPage() {
+import { client } from "@/sanity/lib/client"
+
+interface Props {
+  params: {
+    slug: string
+  }
+}
+
+async function getPost(slug: string) {
+
+  const query = `
+  *[
+    _type == "post" &&
+    slug.current == $slug
+  ][0]{
+    title,
+    excerpt
+  }
+  `
+
+  return await client.fetch(
+    query,
+    { slug }
+  )
+}
+
+export default async function BlogPostPage({
+  params,
+}: Props) {
+
+  const post =
+    await getPost(params.slug)
 
   return (
 
     <div
       style={{
         padding: "40px",
-        fontSize: "40px",
       }}
     >
-      BLOG PAGE WORKING
+
+      <h1
+        style={{
+          fontSize: "50px",
+          fontWeight: "bold",
+        }}
+      >
+        {post?.title}
+      </h1>
+
+      <p
+        style={{
+          marginTop: "20px",
+          fontSize: "22px",
+        }}
+      >
+        {post?.excerpt}
+      </p>
+
     </div>
   )
 }
