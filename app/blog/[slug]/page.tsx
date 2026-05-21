25ee0501abe493fd3403f4cacd7f84f21656311c
celestialord01-eng <celestialord01@gmail.com>
2026-05-21 19:02:16 +0530

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import { PortableText } from "@portabletext/react"
+
 import { client } from "@/sanity/lib/client"
 import { urlFor } from "@/sanity/lib/image"
 
@@ -20,46 +21,102 @@ export default async function BlogPostPage({
   )
 
   return (
+
     <div
       style={{
         padding: "40px",
+        maxWidth: "900px",
+        margin: "0 auto",
       }}
     >
+
       <h1
         style={{
-          fontSize: "40px",
-          marginBottom: "30px",
+          fontSize: "48px",
+          marginBottom: "40px",
+          fontWeight: "bold",
         }}
       >
         {post?.title}
       </h1>
 
       <PortableText
-  value={post?.body}
-  components={{
-    types: {
-      image: ({ value }: any) => (
+        value={post?.body}
+        components={{
+          types: {
 
-        <div
-          style={{
-            marginTop: "40px",
-            marginBottom: "40px",
-            position: "relative",
-            width: "100%",
-            height: "500px",
-          }}
-        >
-          <Image
-            src={urlFor(value).url()}
-            alt={value?.alt || "blog"}
-            fill
-            style={{
-              objectFit: "cover",
-              borderRadius: "20px",
-            }}
-          />
-        </div>
-      ),
-    },
-  }}
-/>
+            image: ({ value }: any) => (
+
+              <div
+                style={{
+                  marginTop: "40px",
+                  marginBottom: "40px",
+                  position: "relative",
+                  width: "100%",
+                  height: "500px",
+                }}
+              >
+
+                <Image
+                  src={urlFor(value).url()}
+                  alt={value?.alt || "blog"}
+                  fill
+                  style={{
+                    objectFit: "cover",
+                    borderRadius: "20px",
+                  }}
+                />
+
+              </div>
+            ),
+          },
+
+          block: {
+
+            normal: ({ children }) => (
+
+              <p
+                style={{
+                  fontSize: "20px",
+                  lineHeight: "2",
+                  marginBottom: "30px",
+                }}
+              >
+                {children}
+              </p>
+            ),
+
+            h2: ({ children }) => (
+
+              <h2
+                style={{
+                  fontSize: "36px",
+                  marginTop: "60px",
+                  marginBottom: "20px",
+                  fontWeight: "bold",
+                }}
+              >
+                {children}
+              </h2>
+            ),
+
+            h3: ({ children }) => (
+
+              <h3
+                style={{
+                  fontSize: "28px",
+                  marginTop: "40px",
+                  marginBottom: "20px",
+                  fontWeight: "bold",
+                }}
+              >
+                {children}
+              </h3>
+            ),
+          },
+        }}
+      />
+
+    </div>
+  )
+}
