b7db896182744975b560741324869b814b224acf
celestialord01-eng <celestialord01@gmail.com>
2026-05-21 18:20:36 +0530

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,13 +1,31 @@
 
-          export default function BlogPostPage() {
+          import { client } from "@/sanity/lib/client"
+
+async function getPost(slug: string) {
+  return await client.fetch(
+    `*[_type == "post" && slug.current == $slug][0]{
+      title
+    }`,
+    { slug }
+  )
+}
+
+export default async function BlogPostPage({
+  params,
+}: {
+  params: { slug: string }
+}) {
+
+  const post = await getPost(params.slug)
+
   return (
     <div
       style={{
         padding: "40px",
-        fontSize: "32px",
+        fontSize: "30px",
       }}
     >
-      BLOG SLUG PAGE WORKING
+      {post?.title || "NO POST"}
     </div>
   )
-          }
+}
