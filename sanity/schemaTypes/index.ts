c53a74104bcfb151237cf8c1f95fc3732e29dba9
celestialord01-eng <celestialord01@gmail.com>
2026-05-22 16:02:24 +0530

    Update index.ts

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -2,6 +2,7 @@ import post from "./post"
 import category from "./category"
 import amazonFind from "./amazonFind"
 import hero from "./hero"
+import customImage from "./customImage"
 import aboutPage from "./aboutPage"
 import { callout } from "./objects/callout"
 export const schemaTypes = [
@@ -10,5 +11,6 @@ export const schemaTypes = [
   amazonFind,
   hero,
   callout,
+  customImage,
   aboutPage,
 ]
