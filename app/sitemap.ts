e227e4b8a3c637843b732f95da9969ee4f8abb34
celestialord01-eng <celestialord01@gmail.com>
2026-05-26 23:54:08 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -10,13 +10,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: 'https://velvet-blog.vercel.app/blog',
+      url: 'https://velvetnestblog.vercel.app/blog',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.9,
     },
     {
-      url: 'https://velvet-blog.vercel.app/about',
+      url: 'https://velvetnestblog.vercel.app/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
