ad17853bed712cd1456e9078518be3dd435a73fd
celestialord01-eng <celestialord01@gmail.com>
2026-05-26 23:53:38 +0530

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://velvet-blog.vercel.app/sitemap.xml',
+    sitemap: 'https://velvetnestblog.vercel.app/sitemap.xml',
   }
 }
