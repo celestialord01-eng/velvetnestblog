b210dd4a630b5d81dc6eeb2e4628b18166306bb2
celestialord01-eng <celestialord01@gmail.com>
2026-05-27 00:12:02 +0530

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,23 @@
 
 import { MetadataRoute } from 'next'
+import { client } from '@/sanity/lib/client'
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+
+  const posts = await client.fetch(`
+    *[_type == "post"]{
+      "slug": slug.current,
+      _updatedAt
+    }
+  `)
+
+  const postUrls = posts.map((post: any) => ({
+    url: `https://velvetnestblog.vercel.app/blog/${post.slug}`,
+    lastModified: new Date(post._updatedAt),
+    changeFrequency: 'weekly' as const,
+    priority: 0.8,
+  }))
+
   return [
     {
       url: 'https://velvetnestblog.vercel.app',
@@ -9,17 +25,21 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'daily',
       priority: 1,
     },
+
     {
       url: 'https://velvetnestblog.vercel.app/blog',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.9,
     },
+
     {
       url: 'https://velvetnestblog.vercel.app/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.7,
     },
+
+    ...postUrls,
   ]
-    }
+        }
