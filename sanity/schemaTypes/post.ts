cad31b62df60d3bc112b39ce647bc9262e35b902
celestialord01-eng <celestialord01@gmail.com>
2026-05-29 12:55:41 +0530

    Update post.ts

--- sanity/schemaTypes/post.ts
+++ sanity/schemaTypes/post.ts
@@ -172,53 +172,10 @@ export default defineType({
           },
         },
 
-        // IMAGE BLOCK
+// Image Block
 {
-  type: "image",
-
-  options: {
-    hotspot: true,
-  },
-
-  fields: [
-    defineField({
-      name: "alt",
-      title: "Alt Text",
-      type: "string",
-      validation: (Rule) => Rule.required(),
-    }),
-
-    defineField({
-      name: "caption",
-      title: "Caption",
-      type: "string",
-    }),
-
-    defineField({
-      name: "layout",
-      title: "Image Layout",
-      type: "string",
-
-      initialValue: "pinterest",
-
-      options: {
-        list: [
-          {
-            title: "Pinterest (2:3)",
-            value: "pinterest",
-          },
-          {
-            title: "Full Width",
-            value: "fullWidth",
-          },
-        ],
-
-        layout: "radio",
-      },
-    }),
-  ],
+  type: "customImage",
 },
-
         // LINK BLOCK
         {
           name: "linkBlock",
