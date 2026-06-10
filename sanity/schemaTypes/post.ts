a4a861fa04d0f4dd4f6fbe9c1bfc290a6829b47b
celestialord01-eng <celestialord01@gmail.com>
2026-06-10 16:06:07 +0530

    Add*ng import

--- sanity/schemaTypes/post.ts
+++ sanity/schemaTypes/post.ts
@@ -1,5 +1,5 @@
 import { defineField, defineType } from "sanity"
-
+import MarkdownImportButton from "../components/MarkdownImportButton"
 export default defineType({
   name: "post",
   title: "Post",
