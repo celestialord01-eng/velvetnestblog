bbbeed9db6d5ed3a280b06e5069f881a3b0fe54a
celestialord01-eng <celestialord01@gmail.com>
2026-07-05 01:01:38 +0530

    Add createTable function to create table objects

--- sanity/lib/helpers/createTable.ts
+++ sanity/lib/helpers/createTable.ts
@@ -1,1 +1,11 @@
+import { key } from "./key"
 
+export function createTable(rows: any[]) {
+  return {
+    _key: key(),
+    _type: "table",
+    caption: "",
+    hasHeader: true,
+    rows,
+  }
+}
