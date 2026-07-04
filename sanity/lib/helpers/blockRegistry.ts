4e8cb62a15a0fe35f811b69862999651795140c3
celestialord01-eng <celestialord01@gmail.com>
2026-07-05 02:34:29 +0530

    Add button converter to block registry

--- sanity/lib/helpers/blockRegistry.ts
+++ sanity/lib/helpers/blockRegistry.ts
@@ -1,7 +1,13 @@
 import { convertCallout } from "../converters/callout"
 import { convertImage } from "../converters/image"
+import { convertButton } from "../converters/button"
 
 export const blockRegistry = {
+
   callout: convertCallout,
+
   image: convertImage,
+
+  button: convertButton,
+
 }
