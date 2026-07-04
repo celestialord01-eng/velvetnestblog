4207f8a9f5755014ff1be4322f59ab00f0770d5d
celestialord01-eng <celestialord01@gmail.com>
2026-07-05 02:07:00 +0530

    Add converters to blockRegistry

--- sanity/lib/helpers/blockRegistry.ts
+++ sanity/lib/helpers/blockRegistry.ts
@@ -1,1 +1,7 @@
-export const blockRegistry = {}
+import { convertCallout } from "../converters/callout"
+import { convertImage } from "../converters/image"
+
+export const blockRegistry = {
+  callout: convertCallout,
+  image: convertImage,
+}
