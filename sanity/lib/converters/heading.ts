cf2498586abed58a6739d90701513ee6cfe1e640
celestialord01-eng <celestialord01@gmail.com>
2026-07-05 01:19:16 +0530

    Implement heading conversion with depth styling

--- sanity/lib/converters/heading.ts
+++ sanity/lib/converters/heading.ts
@@ -1,1 +1,36 @@
 
+import { createBlock } from "../helpers/createBlock"
+import { parseChildren } from "../helpers/parseChildren"
+
+export function convertHeading(node: any) {
+  const parsed = parseChildren(node.children)
+
+  let style = "normal"
+
+  switch (node.depth) {
+    case 1:
+      style = "h1"
+      break
+
+    case 2:
+      style = "h2"
+      break
+
+    case 3:
+      style = "h3"
+      break
+
+    case 4:
+      style = "h4"
+      break
+
+    default:
+      style = "normal"
+  }
+
+  return createBlock(
+    style,
+    parsed.children,
+    parsed.markDefs
+  )
+}
