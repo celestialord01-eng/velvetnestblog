de07b4690b074fbc03ad62b725468d2047571762
celestialord01-eng <celestialord01@gmail.com>
2026-07-05 02:56:39 +0530

    Clean up markdownToPortableText.ts by removing dead code
    Removed unused imports and commented-out code related to custom block parsing.

--- sanity/lib/markdownToPortableText.ts
+++ sanity/lib/markdownToPortableText.ts
@@ -1,14 +1,6 @@
 import { remark } from "remark"
 import remarkParse from "remark-parse"
 import remarkGfm from "remark-gfm"
-import { parseChildren } from "./helpers/parseChildren"
-import { convertHeading } from "./converters/heading"
-import { convertParagraph } from "./converters/paragraph"
-import { convertQuote } from "./converters/quote"
-import { convertList } from "./converters/list"
-import { convertTable } from "./converters/table"
-import { convertImage } from "./converters/image"
-import { markdownRegistry } from "./helpers/markdownRegistry"
 import { convertMarkdownNode } from "./helpers/convertMarkdownNode"
 import { extractText } from "./helpers/extractText"
 import { parseCustomBlock } from "./helpers/parseCustomBlock"
@@ -54,22 +46,7 @@ const tree = remark()
 
     continue
   }
-  }
-
-    {
-
-      const parsed = parseCustomBlock(text)
-
-      const converted =
-        convertCustomBlock(parsed)
-
-      if (converted) {
-        blocks.push(converted)
-      }
-
-      continue
-    }
-  }
+  
 
   // Standard Markdown
   const converted =
