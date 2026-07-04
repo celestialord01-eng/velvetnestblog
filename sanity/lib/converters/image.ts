b3e4df4c636475d7a112add4f49f67291b001840
celestialord01-eng <celestialord01@gmail.com>
2026-07-05 01:43:08 +0530

    Add convertImage function for image conversion

--- sanity/lib/converters/image.ts
+++ sanity/lib/converters/image.ts
@@ -1,1 +1,14 @@
+import { key } from "../helpers/key"
 
+export function convertImage(node: any) {
+  return {
+    _key: key(),
+    _type: "image",
+
+    alt: node.alt || "",
+
+    caption: node.title || "",
+
+    asset: undefined,
+  }
+}
