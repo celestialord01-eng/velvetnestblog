b5986978675e7e62c7ee389cf4b43eee4a96a795
celestialord01-eng <celestialord01@gmail.com>
2026-07-04 16:49:18 +0530

    Add table, tableRow, and tableCell to schemaTypes

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -5,11 +5,17 @@ import hero from "./hero"
 import aboutPage from "./aboutPage"
 import storefrontCTA from "./storefrontCTA"
 import { callout } from "./objects/callout"
+import table from './table'
+import tableRow from './tableRow'
+import tableCell from './tableCell'
 export const schemaTypes = [
   post,
   category,
   amazonFind,
   hero,
+  table,
+tableRow,
+tableCell,
   storefrontCTA,
   callout,
   aboutPage,
