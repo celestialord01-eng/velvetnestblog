c0a9cdd3c55e9a9183dcc24ccbed5f864cd1910f
celestialord01-eng <celestialord01@gmail.com>
2026-07-05 01:26:12 +0530

    Add convertQuote function to handle quote nodes

--- sanity/lib/converters/quote.ts
+++ sanity/lib/converters/quote.ts
@@ -1,1 +1,17 @@
 
+import { createBlock } from "../helpers/createBlock"
+import { parseChildren } from "../helpers/parseChildren"
+
+export function convertQuote(node: any) {
+  const paragraph = node.children?.[0]
+
+  const parsed = parseChildren(
+    paragraph?.children || []
+  )
+
+  return createBlock(
+    "blockquote",
+    parsed.children,
+    parsed.markDefs
+  )
+}
