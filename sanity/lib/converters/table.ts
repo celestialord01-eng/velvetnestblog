72828937fd5fd17f4b2ef7dadfdf45da8c944224
celestialord01-eng <celestialord01@gmail.com>
2026-07-05 01:31:11 +0530

    Implement convertTable function for table conversion

--- sanity/lib/converters/table.ts
+++ sanity/lib/converters/table.ts
@@ -1,1 +1,35 @@
+import { key } from "../helpers/key"
+import { createTable } from "../helpers/createTable"
+import { parseChildren } from "../helpers/parseChildren"
 
+export function convertTable(node: any) {
+  const rows = node.children.map((row: any) => ({
+    _key: key(),
+    _type: "tableRow",
+
+    cells: row.children.map((cell: any) => {
+      const parsed = parseChildren(cell.children)
+
+      return {
+        _key: key(),
+        _type: "tableCell",
+
+        align: "left",
+
+        content: [
+          {
+            _key: key(),
+            _type: "block",
+            style: "normal",
+
+            children: parsed.children,
+
+            markDefs: parsed.markDefs,
+          },
+        ],
+      }
+    }),
+  }))
+
+  return createTable(rows)
+}
