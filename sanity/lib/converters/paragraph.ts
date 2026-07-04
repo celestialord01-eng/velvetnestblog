c97f8dda22606e9d3f7a670d417a5c635de81217
celestialord01-eng <celestialord01@gmail.com>
2026-07-05 01:22:22 +0530

    Implement convertParagraph function for node conversion

--- sanity/lib/converters/paragraph.ts
+++ sanity/lib/converters/paragraph.ts
@@ -1,1 +1,13 @@
 
+import { createBlock } from "../helpers/createBlock"
+import { parseChildren } from "../helpers/parseChildren"
+
+export function convertParagraph(node: any) {
+  const parsed = parseChildren(node.children)
+
+  return createBlock(
+    "normal",
+    parsed.children,
+    parsed.markDefs
+  )
+}
